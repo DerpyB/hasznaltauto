a59d2d6bdbc3550456ba4260a2fbece989f09ae4
DerpyB <balazs10lecz@gmail.com>
2026-03-17 18:34:57 +0200

    dasdadad

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/app/',
   build: {
-    outDir: "dist"
+    outDir: "../backend/public/app"
   },
   plugins: [
     vue(),
